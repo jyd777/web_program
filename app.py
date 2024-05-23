65af1731ed47c10892a39f71518eb5d631b66279
jyd777 <163747737+jyd777@users.noreply.github.com>
2024-05-24 01:22:41 +0800

    词书
    加了孙jy的单词背诵和错题本；在用户注册时会初始化单词熟练度

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, session, url_for, flash, jsonify
-import pymysql,base64,os,hashlib
-from datetime import datetime
+import pymysql,base64,os,hashlib,random
 
 app = Flask(__name__)
 app.secret_key = '123'
@@ -16,7 +15,7 @@ def login():
         connection = pymysql.connect(
             host="localhost",
             user="root",
-            password="Byj20040720",
+            password="密码",
             database="web_program"
         )
         cursor = connection.cursor()
@@ -28,13 +27,13 @@ def login():
         if result:
             # 获取数据库中存储的哈希密码
             hashed_password = result[2]
-            print(result[2])
             # 对用户输入的密码进行哈希处理
             input_hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
             if hashed_password == input_hashed_password:
                 # 验证通过，跳转到home页
                 session["username"] = username
+                session["user_id"]=result[0]
                 return jsonify({'message': '登录成功'})
             else:
                 # 验证失败，返回登录页面并显示错误信息
@@ -53,6 +52,7 @@ def login():
 @app.route('/logout', methods=['POST'])
 def logout():
     session.pop('username', None)  # 清空会话中的用户名信息
+    session.pop('user_id', None)
     return '退出登录成功'
 # 注册界面
 # 功能：输入用户的用户名、邮箱、密码和确认密码，对用户名是否存在、密码和确认密码是否匹配进行检查
@@ -69,7 +69,7 @@ def register():
         connection = pymysql.connect(
             host="localhost",
             user="root",
-            password="Byj20040720",
+            password="密码",
             database="web_program"
         )
         # 建立游标，用于后续的mysql操纵
@@ -85,6 +85,14 @@ def register():
         # 将用户名和密码导入到数据库
         query = "INSERT INTO users (username, password) VALUES (%s, %s)"
         cursor.execute(query, (username, hashed_password))
+        # 提交事务
+        connection.commit()
+        # 获取新用户的ID
+        user_id = cursor.lastrowid
+        
+        # 从words表中获取所有的Wordname，并将新用户的ID与每个单词的名称和熟练度0一起插入到wordprogress表中
+        query = "INSERT INTO wordprogress (user_id, Wordname, Familiarity) SELECT %s, Wordname, 0 FROM words"
+        cursor.execute(query, (user_id,))
         # 提交事务
         connection.commit()
         # 关闭数据库连接
@@ -106,7 +114,7 @@ def execute_user_info(username):
     connection = pymysql.connect(
         host="localhost",
         user="root",
-        password="Byj20040720",
+        password="密码",
         database="web_program"
     )
     # 建立游标，用于后续的mysql操纵
@@ -158,7 +166,7 @@ def update_data():
     connection = pymysql.connect(
         host="localhost",
         user="root",
-        password="Byj20040720",
+        password="密码",
         database="web_program"
     )
     cursor = connection.cursor()
@@ -184,7 +192,7 @@ def uploadimage():
     connection = pymysql.connect(
         host="localhost",
         user="root",
-        password="Byj20040720",
+        password="密码",
         database="web_program"
     )
     username = request.form.get('username')
@@ -204,7 +212,7 @@ def upload_bg_image():
     connection = pymysql.connect(
         host="localhost",
         user="root",
-        password="Byj20040720",
+        password="密码",
         database="web_program"
     )
     username = request.form.get('username')
@@ -222,7 +230,7 @@ def connect_db():
     return pymysql.connect(
         host="localhost",
         user="root",
-        password="Byj20040720",  
+        password="密码",  
         database="web_program"
     )
 
@@ -232,7 +240,7 @@ def close_db(connection):
         connection.close()
 
 # 主页路由
-@app.route('/')
+@app.route('/word_learning')
 def index():
     """
     主页路由
@@ -240,73 +248,87 @@ def index():
     当用户访问网站主页时，返回单词学习的 HTML 页面。
     """
     return render_template('word_learning.html')
-
+@app.route('/help')
+def help_page():
+    return render_template('Help.html')
 # 启动学习流程的路由
 @app.route('/start_learning', methods=['POST'])
 def start_learning():
-    """
-    启动学习流程的路由
-
-    当用户开始学习时，从数据库中获取第一个单词，并返回给用户。
-
-    如果找到单词，返回 JSON 响应包含单词名称。
-    如果未找到单词，返回 JSON 响应包含错误信息和 404 状态码。
-    """
     db = connect_db()
     cursor = db.cursor()
-    user_id = session.get('user_id', '1')  # 用实际的用户标识替换
-    cursor.execute("SELECT Wordname FROM Words LIMIT 1")
-    word = cursor.fetchone()
-    close_db(db)
-    if word:
-        return jsonify({'word': word[0]})
-    else:
-        return jsonify({'error': '未找到单词'}), 404
-# 启动学习流程的路由，从数据库中获取单词
-@app.route('/start_learning_db', methods=['POST'])
-def start_learning_db():
-    """
-    启动学习流程的路由，从数据库中获取单词
-
-    当用户开始学习时，从数据库中获取第一个单词，并返回给用户。
-
-    如果找到单词，返回 JSON 响应包含单词名称。
-    如果未找到单词，返回 JSON 响应包含错误信息和 404 状态码。
-    """
-    db = connect_db()
-    cursor = db.cursor()
-    user_id = session.get('user_id', '1')  # 用实际的用户标识替换
+    user_id = session.get('user_id')  # 用实际的用户标识替换
+    print(user_id)
     cursor.execute("SELECT Wordname FROM Words LIMIT 1")
     word = cursor.fetchone()
     cursor.execute("SELECT Familiarity FROM WordProgress WHERE Wordname = %s AND user_id = %s", (word, user_id))
     Fa = cursor.fetchone()
+    cursor.execute("SELECT Expression FROM Expressions WHERE Wordname = %s", (word,))
+    correct_definition = cursor.fetchone()
+    # 获取其他错误释义
+    cursor.execute("SELECT Expression FROM Expressions WHERE Wordname != %s ORDER BY RAND() LIMIT 2", (word,))
+    wrong_definitions = cursor.fetchall()
+    options = [correct_definition[0], wrong_definitions[0][0], wrong_definitions[1][0]]
+    random.shuffle(options)
     close_db(db)
     if word:
-        return jsonify({'word': word[0],'familiarity': Fa[0]})
+        return jsonify({
+            'word': word[0],
+            'familiarity': Fa[0],
+            'definitions': options,
+            'correct_definition': correct_definition[0]
+        })
+    else:
+        return jsonify({'error': '未找到单词'}), 404
+
+@app.route('/wrongword_db', methods=['POST'])
+def wrongword_db():
+    db = connect_db()
+    cursor = db.cursor()
+    user_id = session.get('user_id')  # 用实际的用户标识替换
+    cursor.execute("SELECT Wordname FROM WordProgress WHERE (Familiarity = 0 OR Familiarity = 1) AND user_ID = %s LIMIT 1", (user_id,))
+    word = cursor.fetchone()
+    if not word:
+        close_db(db)
+        return jsonify({'error': '恭喜，目前暂无错题！'})
+    cursor.execute("SELECT Familiarity FROM WordProgress WHERE Wordname = %s AND user_id = %s", (word, user_id))
+    Fa = cursor.fetchone()
+    cursor.execute("SELECT Expression FROM Expressions WHERE Wordname = %s", (word,))
+    correct_definition = cursor.fetchone()
+    # 获取其他错误释义
+    cursor.execute("SELECT Expression FROM Expressions WHERE Wordname != %s ORDER BY RAND() LIMIT 2", (word,))
+    wrong_definitions = cursor.fetchall()
+    options = [correct_definition[0], wrong_definitions[0][0], wrong_definitions[1][0]]
+    random.shuffle(options)
+    close_db(db)
+    if word:
+        return jsonify({
+            'word': word[0],
+            'familiarity': Fa[0],
+            'definitions': options,
+            'correct_definition': correct_definition[0]
+        })
     else:
         return jsonify({'error': '未找到单词'}), 404
 
 # 获取下一个要学习的单词的路由
 @app.route('/next_word', methods=['POST'])
 def next_word():
+    nowword = request.json.get('word')
     db = connect_db()
     cursor = db.cursor()
-    user_id = session.get('user_id', '1')  # 用实际的用户标识替换
-
-    # 获取当前词
-    current_word = session.get('current_word')
-
-    # 获取所有单词
+    user_id = session.get('user_id')
+    print(user_id)
+    current_word=nowword
+    print(current_word)
     cursor.execute("SELECT Wordname FROM Words ORDER BY Wordname")
     all_words = cursor.fetchall()
     all_words = [word[0] for word in all_words]
-
+    print(all_words)
     if not all_words:
         close_db(db)
         return jsonify({'error': '未找到单词'}), 404
 
     if current_word is None:
-        # 如果没有当前词，返回第一个单词
         next_word = all_words[0]
     else:
         try:
@@ -314,27 +336,94 @@ def next_word():
             next_index = (current_index + 1) % len(all_words)
             next_word = all_words[next_index]
         except ValueError:
-            # 当前词不在所有单词列表中，返回第一个单词
             next_word = all_words[0]
 
-    # 更新会话中的当前词
     session['current_word'] = next_word
-
-    # 获取单词详情
+    print(next_word)
     cursor.execute("SELECT PT FROM Words WHERE Wordname = %s", (next_word,))
     pt = cursor.fetchone()
     cursor.execute("SELECT Expression, ES FROM Expressions WHERE Wordname = %s", (next_word,))
     expressions = cursor.fetchall()
     cursor.execute("SELECT Familiarity FROM WordProgress WHERE Wordname = %s AND user_id = %s", (next_word, user_id))
     Fa = cursor.fetchone()
-    
-    close_db(db)
+    print(Fa)
+    # 获取正确释义
+    cursor.execute("SELECT Expression FROM Expressions WHERE Wordname = %s", (next_word,))
+    correct_definition = cursor.fetchone()
 
+    # 获取其他错误释义
+    cursor.execute("SELECT Expression FROM Expressions WHERE Wordname != %s ORDER BY RAND() LIMIT 2", (next_word,))
+    wrong_definitions = cursor.fetchall()
+
+    options = [correct_definition[0], wrong_definitions[0][0], wrong_definitions[1][0]]
+    random.shuffle(options)
+
+    close_db(db)
     if pt and expressions:
         return jsonify({
             'word': next_word,
             'PT': pt[0],
-            'familiarity':Fa[0],
+            'familiarity': Fa[0],
+            'definitions': options,
+            'correct_definition': correct_definition[0],
+            'expressions': [{'expression': exp[0], 'es': exp[1]} for exp in expressions]
+        })
+    else:
+        return jsonify({'error': '单词详情加载失败'}), 404
+    
+@app.route('/nextwrongword', methods=['POST'])
+def nextwrongword():
+    nowword = request.json.get('word')
+    db = connect_db()
+    cursor = db.cursor()
+    user_id = session.get('user_id')
+    current_word=nowword
+    cursor.execute("SELECT Wordname FROM WordProgress WHERE (Familiarity = 0 OR Familiarity = 1) AND user_id = %s ORDER BY Wordname", (user_id,))
+    all_words = cursor.fetchall()
+    all_words = [word[0] for word in all_words]
+
+    if not all_words:
+        close_db(db)
+        return jsonify({'error': '恭喜，目前暂无错题！'})
+
+    if current_word is None:
+        next_word = all_words[0]
+    else:
+        try:
+            current_index = all_words.index(current_word)
+            next_index = (current_index + 1) % len(all_words)
+            next_word = all_words[next_index]
+        except ValueError:
+            next_word = all_words[0]
+
+    session['current_word'] = next_word
+
+    cursor.execute("SELECT PT FROM Words WHERE Wordname = %s", (next_word,))
+    pt = cursor.fetchone()
+    cursor.execute("SELECT Expression, ES FROM Expressions WHERE Wordname = %s", (next_word,))
+    expressions = cursor.fetchall()
+    cursor.execute("SELECT Familiarity FROM WordProgress WHERE Wordname = %s AND user_id = %s", (next_word, user_id))
+    Fa = cursor.fetchone()
+
+    # 获取正确释义
+    cursor.execute("SELECT Expression FROM Expressions WHERE Wordname = %s", (next_word,))
+    correct_definition = cursor.fetchone()
+
+    # 获取其他错误释义
+    cursor.execute("SELECT Expression FROM Expressions WHERE Wordname != %s ORDER BY RAND() LIMIT 2", (next_word,))
+    wrong_definitions = cursor.fetchall()
+
+    options = [correct_definition[0], wrong_definitions[0][0], wrong_definitions[1][0]]
+    random.shuffle(options)
+
+    close_db(db)
+    if pt and expressions:
+        return jsonify({
+            'word': next_word,
+            'PT': pt[0],
+            'familiarity': Fa[0],
+            'definitions': options,
+            'correct_definition': correct_definition[0],
             'expressions': [{'expression': exp[0], 'es': exp[1]} for exp in expressions]
         })
     else:
@@ -344,43 +433,36 @@ def next_word():
 # 更新用户对单词的熟悉度的路由
 @app.route('/update_familiarity', methods=['POST'])
 def update_familiarity():
-    """
-    更新用户对单词的熟悉度的路由
-
-    当用户更新对单词的熟悉度时，将用户对单词的熟悉度存储到数据库中。
-
-    如果参数缺失，返回 JSON 响应包含错误信息和 400 状态码。
-    如果更新成功，返回 JSON 响应包含成功信息。
-    """
     data = request.get_json()
-    familiarity = data.get('familiarity')
     word = data.get('word')
+    correct_definition = data.get('correct_definition')
+    selected_definition = data.get('selected_definition')
 
-    if familiarity is None or word is None:
+    if word is None or correct_definition is None or selected_definition is None:
         return jsonify({'error': '缺少参数'}), 400
 
     db = connect_db()
     cursor = db.cursor()
-    user_id = session.get('user_id', '1')  # 用实际的用户标识替换
-    cursor.execute("REPLACE INTO WordProgress (user_id, Wordname, Familiarity) VALUES (%s, %s, %s)",
-                   (user_id, word, familiarity))
+    user_id = session.get('user_id')
+
+    if selected_definition == correct_definition:
+        cursor.execute("UPDATE WordProgress SET Familiarity = LEAST(Familiarity + 1, 2) WHERE user_id = %s AND Wordname = %s", (user_id, word))
+    else:
+        cursor.execute("UPDATE WordProgress SET Familiarity = GREATEST(Familiarity - 1, 0) WHERE user_id = %s AND Wordname = %s", (user_id, word))
+    cursor.execute("SELECT Familiarity FROM WordProgress WHERE Wordname = %s AND user_id = %s", (word, user_id))
+    Fa = cursor.fetchone()
+
     db.commit()
     close_db(db)
-    return jsonify({'message': '熟悉度更新成功'})
-
+    return jsonify({
+            'word' : word,
+            'familiarity':Fa[0]
+        })
+    
 # 搜索单词的路由
 # 修改搜索单词的路由
 @app.route('/search_word', methods=['POST'])
 def search_word():
-    """
-    搜索单词的路由
-
-    当用户搜索单词时，从数据库中查找单词并返回其熟悉度。
-
-    如果缺少搜索词参数，返回 JSON 响应包含错误信息和 400 状态码。
-    如果找到单词，返回 JSON 响应包含单词名称、熟悉度和详细信息。
-    如果未找到单词，返回 JSON 响应包含错误信息和 404 状态码。
-    """
     data = request.get_json()
     search_word = data.get('word')
 
@@ -389,7 +471,7 @@ def search_word():
 
     db = connect_db()
     cursor = db.cursor()
-    user_id = session.get('user_id', '1')  # 用实际的用户标识替换
+    user_id = session.get('user_id')  # 用实际的用户标识替换
     cursor.execute("SELECT w.Wordname, IFNULL(wp.Familiarity, 0) FROM Words w LEFT JOIN WordProgress wp ON w.Wordname = wp.Wordname AND wp.user_id = %s WHERE w.Wordname = %s",
                    (user_id, search_word))
     result = cursor.fetchone()
@@ -432,7 +514,12 @@ def get_word_details():
         return jsonify({'PT': word_PT[0],'expression': word_details[0], 'example_sentence': word_details[1], 'example_sentence_c': word_details[2]})
     else:
         return jsonify({'error': '未找到单词详细信息'}), 404
-        
+
+# 跳转到词书选择页
+@app.route('/word_book_choose')
+def word_book_choose():
+    return render_template('word_book_choose.html')
+
 #论坛的个人主页，显示该用户所有帖子的信息
 @app.route('/person',methods=['GET'])
 def person():
@@ -441,7 +528,7 @@ def person():
     connection = pymysql.connect(
         host="localhost",
         user="root",
-        password="Byj20040720",
+        password="密码",
         database="web_program"
     )
     # 建立游标，用于后续的mysql操纵
@@ -468,7 +555,7 @@ def blogcomments():
     connection = pymysql.connect(
         host="localhost",
         user="root",
-        password="Byj20040720",
+        password="密码",
         database="web_program"
     )
     if request.method=='GET':
@@ -502,7 +589,7 @@ def upload():
         connection = pymysql.connect(
             host="localhost",
             user="root",
-            password="Byj20040720",
+            password="密码",
             database="web_program"
         )
         # 建立游标，用于后续的mysql操纵
@@ -538,7 +625,7 @@ def all_blogs():
     connection = pymysql.connect(
         host="localhost",
         user="root",
-        password="Byj20040720",
+        password="密码",
         database="web_program"
     )
     # 建立游标，用于后续的mysql操纵
